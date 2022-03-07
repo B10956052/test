be4785c8f6289d04e95e4e3229eb86febed05346
user <user@DESKTOP-6JBEF5K>
2022-03-07 11:09:48 +0800

    完成練習

--- test/Program.cs
+++ test/Program.cs
@@ -10,7 +10,8 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("測試練習");
         }
     }
 }
